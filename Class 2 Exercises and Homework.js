655f2c46d404e5164cfb97a9ad22488eabf4053b
Parksiri Treeyuthakul <petetree@uw.edu>
2023-10-18 13:42:51 -0700

    Add Code for Class 2 Excercises

--- Class 2 Exercises and Homework.js
+++ Class 2 Exercises and Homework.js
@@ -5,22 +5,43 @@
 // - 'favorite food'
 // - bestFriend (object with the same 3 properties as above)
 
+const me = {
+  firstName : 'Pete',
+  lastName : 'Treeyuthakul',
+  'favorite food' : 'Padthai',
+  bestFriend : {
+    firstName : 'Batman',
+    lastName : 'Wayne',
+    'favorite food' : 'Joker Pie',
+  }
+}
 
 // 2. console.log best friend's firstName and your favorite food
 
+console.log(`My best friend's first name is ${me.bestFriend.firstName}.`)
+console.log(`My favorite food is ${me['favorite food']}.`)
 
 // 3. Create an array to represent this tic-tac-toe board
 // -O-
 // -XO
 // X-X
 
+const ticTacToe =[
+  ['-','o','-'],
+  ['-','x','o'],
+  ['x','-','x']
+]
 
 // 4. After the array is created, 'O' claims the top right square.
 // Update that value.
 
+ticTacToe[0][2]='o'
 
 // 5. Log the grid to the console.
 
+console.log(ticTacToe[0])
+console.log(ticTacToe[1])
+console.log(ticTacToe[2])
 
 // 6. You are given an email as string myEmail, make sure it is in correct email format.
 // Should be 1 or more characters, then @ sign, then 1 or more characters, then dot, then one or more characters - no whitespace
@@ -29,16 +50,23 @@
 // - Use rubular to check a few emails: https://rubular.com/
 // - Use regexp test method https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/test
 
+const myEmail = 'petetree@uw.edu'
+const regexEmail = /\w\S+@+\w\S+.+\w\S+$/
+
+console.log(regexEmail.test(myEmail))
 
 // 7. You are given an assignmentDate as a string in the format "month/day/year"
 // i.e. '1/21/2019' - but this could be any date.
 // Convert this string to a Date
 const assignmentDate = '1/21/2019';
-
+const assignDate= new Date(assignmentDate)
 
 // 8. Create a new Date instance to represent the dueDate.  
 // This will be exactly 7 days after the assignment date.
 
+let dueDate = assignDate 
+
+dueDate.setDate(assignDate.getDate()+7)
 
 // 9. Use dueDate values to create an HTML time tag in format
 // <time datetime="YYYY-MM-DD">Month day, year</time>
@@ -58,5 +86,8 @@ const months = [
   'December'
 ];
 
+const strdateTime = `${dueDate.getFullYear()}-${dueDate.getMonth()+1}-${dueDate.getDate()}`
+console.log(strdateTime)
 
 // 10. log this value using console.log
+console.log(`<time datetime= "${strdateTime}">${months[dueDate.getMonth()]} ${dueDate.getDate()}, ${dueDate.getFullYear()}</time>`)
